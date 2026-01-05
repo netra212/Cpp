9f97d13006f72897ed9c5250ffb82b11062f3a8b
netra212 <netra200021kcbdr@gmail.com>
2026-01-05 11:36:19 +0000

    selection sort

---
+++ 01_Arrays/04_Selection_Sort.cpp
@@ -0,0 +1,21 @@
+/**
+ * Selection Sort.
+ * arr = [3, 8, 1, 2, 5]
+ *
+ */
+
+#include <iostream>
+using namespace std;
+// Link to study selection sort: https://www.programiz.com/dsa/selection-sort
+
+// sorting in increasing order.
+void sortIncreasingOrder(int arr[], int n)
+{
+    //
+}
+
+// main function.
+int main()
+{
+    //
+}
