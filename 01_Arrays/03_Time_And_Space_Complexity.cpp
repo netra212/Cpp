20b5c16bef82644e6493275fe110685f536b0812
netra212 <netra200021kcbdr@gmail.com>
2026-01-05 11:35:56 +0000

    time and space complexity

---
+++ 01_Arrays/03_Time_And_Space_Complexity.cpp
@@ -0,0 +1,7 @@
+/*
+ * Time and Space Complexity.
+ * Time Complexity: It is the total time taken by an algorithm to run as function of length of the input.
+ * Big(0)   -> Worse Case Time Complexity -> We always calculate this one Because we always have to prepare for the worse case scenarios.
+ * Omega(0) -> Best Case Time Complexity.
+ * Theta    -> Average Case Time Complexity.
+ */
