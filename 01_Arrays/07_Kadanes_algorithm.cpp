9c8f4e09580ca02407ff7b2875b5b3833b1e8f47
netra212 <netra200021kcbdr@gmail.com>
2026-01-06 18:17:43 +0000

    kadane's algorithm

--- 01_Arrays/07_Kadanes_algorithm.cpp
+++ 01_Arrays/07_Kadanes_algorithm.cpp
@@ -3,3 +3,65 @@
  * Kadane's Algorithm
  *
  */
+
+#include <iostream>
+using namespace std;
+
+// Q. Prefix(starting from begining) and Suffix (starting from last) Sum
+void prefixSum(int arr[], int n)
+{
+    vector<int> prefixsum(n);
+    prefixsum[0] = arr[0];
+    for (int i = 1; i < n; i++)
+    {
+        prefixsum[i] = arr[i - 1] + arr[i];
+    }
+}
+
+// Q. Suffix sum
+void suffixSum(int arr[], int n)
+{
+}
+
+// Q. Subarray Problem.
+void printAllSubArray(int arr[], int n)
+{
+    /*
+     *  int arr[] = {4, 3, 7, 2}
+     * 1 size subarray = [4], [3], [7], [2] -> n
+     * 2 size subarray = [4,3], [3,7], [7,2] -> n - 1
+     * 3 size subarray = [4, 3, 7], [3,7,2] -> n-2
+     * 4 size subArray = [4, 3, 7, 2] -> n-3
+     * Question. print all the subArray in the above arrays.
+     */
+}
+
+// Q. Divide array in 2 subarray with equal sum.
+int divideArrayIn2SubarrayWithEqualSum(int arr[], int n)
+{
+    // int arr[] = {3, 4, -2, 5, 8, 20, -10, 8}
+    // Assumption:
+    // totalSum dekhii prefix laii minus gardaii janu paryo.
+    int totalSum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        totalSum += arr[i];
+    }
+    int prefix = 0;
+    for (int i = 0; i < n - 1; i++)
+    {
+        prefix = prefix + arr[i];
+        int ans = totalSum - prefix;
+        if (ans == prefix)
+        {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+//
+int main()
+{
+    int arr[] = {6, 4, 5, -3, 2, 8};
+}
