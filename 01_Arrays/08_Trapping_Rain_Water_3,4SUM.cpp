ab826ea1da59c89c4eb6c17049915dcf3b0a55d2
netra212 <netra200021kcbdr@gmail.com>
2026-01-08 00:57:02 +0000

    Trapping rain water

---
+++ 01_Arrays/08_Trapping_Rain_Water_3,4SUM.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+using namespace std;
+
+// Q. Trapping rain water problem.
+int trappingRainWaterProblem(int arr[], int n)
+{
+    // Height of Building Vs Water stored.
+    // [4, 2, 0, 5, 2, 6, 2, 3]
+    // [0, 2, 4, 0, 3, 0, 1, 0]
+    return 1;
+}
+
+//
+int main()
+{
+
+    return 0;
+}
