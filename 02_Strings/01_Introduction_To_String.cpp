ae6d77a6c5502eb8d463db55c774eb9d2267c5f9
netra212 <netra200021kcbdr@gmail.com>
2026-01-09 17:44:00 +0000

    introduction to string

--- 02_Strings/01_Introduction_To_String.cpp
+++ 02_Strings/01_Introduction_To_String.cpp
@@ -1,4 +1,29 @@
 /**
- *
  * Introduction to strings.
+ * In string, memory allocated in Dynamically.
+ * string stored in heap.
+ * NOTE: string always have a null character at the end of string.
  */
+#include <iostream>
+using namespace std;
+
+// Q. Reverse an string.
+void reverseString(string s)
+{
+    //
+    int start = 0, end = s.size() - 1;
+    while (start < end)
+    {
+        swap(s[start], s[end]);
+        start++, end--;
+    }
+    cout << s;
+}
+
+//
+int main()
+{
+    string s = "Netra kc";
+    // s.push_back('p');
+    // s.pop_back();
+}
