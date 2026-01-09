0eb11ec8642b486627f04513b2c419e0fd753fe1
netra212 <netra200021kcbdr@gmail.com>
2026-01-09 13:58:04 +0000

    Majority Element Count freq

---
+++ 01_Arrays/13_Majority_Elements_count_frq.cpp
@@ -0,0 +1,37 @@
+/***
+ * This file is the part of the Lecture 35 of ther Coder Army youtube channels.
+ * Topic Name: Majority Elements|| Count Frequency of element || Find Missing and Repeating elements.
+ *
+ *
+ */
+#include <iostream>
+using namespace std;
+
+// Q. How to store 2 Numbers in 1 Position.
+void store2NumbersIn1Position()
+{
+    // N = 1 to 99.
+    // store number and it's occurence.
+    // 2, 2, 2, 2, 2, 2.
+    // (2, Occurance)
+
+    // Number = 602 % 100 = 2
+    // Occurence = 602 / 100 = 6
+}
+
+/***
+ * 9 * 39 ->
+ * Buying Rate: 9 || Selling Rate: 13
+ * Front Road Side: 15 * 13 -> 180.
+ * Cost to plot per kathha : 39 - 5
+ * 50 * 13
+ * 20 -> settlement.
+ * 3 Crore 50 Lakhs.
+ * Best scenarios : 20
+ * Worst scenarios : 15
+ */
+
+int main()
+{
+    //
+}
