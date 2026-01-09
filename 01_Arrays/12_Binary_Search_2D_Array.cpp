4f108919620f81a39a7099bce5f776b5402412fe
netra212 <netra200021kcbdr@gmail.com>
2026-01-09 17:32:23 +0000

    Binary search in 2D Arrays

--- 01_Arrays/12_Binary_Search_2D_Array.cpp
+++ 01_Arrays/12_Binary_Search_2D_Array.cpp
@@ -9,9 +9,13 @@
 #include <iostream>
 using namespace std;
 
-void searchElementIn2DArray(vector<vector<int>> &matrix)
+void searchElementIn2DArray(vector<vector<int>> &matrix, int x)
 {
-    //
+    /**
+     * Converting the above 4x5 array into 1-D array.
+     * [2, 6, 10, 14, 18, 20, 24, 27, 29, 38, 47, 52, 78, 93, 102, 108, 111, 200, 218, 320]
+     *
+     */
 }
 
 //
