67a1eb19b05ba404a00181b0f221fc3ff918e672
netra212 <netra200021kcbdr@gmail.com>
2026-01-11 06:38:40 +0000

    adding two strings

--- 02_Strings/04_string_interview_questions_leetcode.cpp
+++ 02_Strings/04_string_interview_questions_leetcode.cpp
@@ -90,16 +90,70 @@ string sortVowelInAString(string &str)
     return str;
 }
 
-// Q. Add string.
-void addString(string &str1, string &str2)
+string add(string num1, string num2)
 {
-    //
+    string ans;
+    int index1 = num1.size() - 1, index2 = num2.size() - 1;
+    int sum, carry = 0;
+
+    // index2 >= 0
+
+    while (index2 >= 0)
+    {
+        sum = (num1[index2] - '0') + (num2[index2] - '0') + carry;
+        carry = sum / 10;
+        char c = '0' + sum % 10;
+        ans += c;
+        index2--, index1--;
+    }
+
+    // index1 >= 0
+    while (index1 >= 0)
+    {
+        sum = (num1[index1] - '0') + carry;
+        carry = sum / 10;
+        char c = '0' + sum % 10;
+        ans += c;
+        index1--;
+    }
+
+    if (carry)
+    {
+        ans += '1';
+    }
+
+    reverse(ans.begin(), ans.end());
+
+    return ans;
 }
 
-int main()
+// Q. Add string.
+string addString(string &num1, string &num2)
 {
-
-    //
+    /**
+     *
+     * num1 = '26583';
+     * num2 = '698';
+     * Assumption: charOfNum1 > charOfNum2
+     * Approach:
+     * Indexing: 0, 1, 2, 3, 4
+     * String 1: 2  6  5  8  3
+     * String 2:       6  9  8
+     * ------------------------
+     * ANSWER  : 2  7  2  8  1
+     */
+    if (num1.size() < num2.size())
+    {
+        return add(num2, num1);
+    }
+    else
+    {
+        return add(num1, num2);
+    }
 }
 
 //
+int main()
+{
+    //
+}
