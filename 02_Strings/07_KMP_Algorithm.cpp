bc95d1ec57ab7f2b3c1abbefb2ce175d813419ee
netra212 <netra200021kcbdr@gmail.com>
2026-01-14 22:41:54 +0000

    KMP Algorithm

--- 02_Strings/07_KMP_Algorithm.cpp
+++ 02_Strings/07_KMP_Algorithm.cpp
@@ -5,6 +5,16 @@
 #include <iostream>
 using namespace std;
 
+// Q. Longest Prefix suffix.
+void LongestPrefixSuffix()
+{
+    /**
+     * Find the longest prefix and suffix from the given example.
+     * Example: ABCDEAABCD
+     * Prefix:
+     */
+}
+
 int main()
 {
     //
