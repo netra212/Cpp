7daf6c0cd2a8a1ffe40a017b50040ee913ce27a1
netra212 <netra200021kcbdr@gmail.com>
2026-01-15 04:57:33 +0000

    KMP Algorithms

--- 02_Strings/07_KMP_Algorithm.cpp
+++ 02_Strings/07_KMP_Algorithm.cpp
@@ -2,19 +2,50 @@
  * KMP Algorithm.
  *
  */
+
 #include <iostream>
 using namespace std;
-
+/**
+ *
+ */
 // Q. Longest Prefix suffix.
-void LongestPrefixSuffix()
+int LongestPrefixSuffix(string &str)
 {
     /**
      * Find the longest prefix and suffix from the given example.
+     * Here is the catch, we cannot take all the string.
      * Example: ABCDEAABCD
      * Prefix:
      */
+
+    vector<int> lps(str.size(), 0);
+    int pre = 0, suf = 1;
+    while (suf < str.size())
+    {
+        // if prefix matched with suffix.
+        if (str[pre] == str[suf])
+        {
+            lps[suf] = pre + 1;
+            suf++, pre++;
+        }
+        else
+        {
+            // if prefix and suffix do not matched.
+            if (pre == 0)
+            {
+                lps[suf] = 0;
+                suf++;
+            }
+            else
+            {
+                pre = lps[pre - 1];
+            }
+        }
+    }
+    return lps[str.size() - 1];
 }
 
+//
 int main()
 {
     //
