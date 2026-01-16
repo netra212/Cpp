b56f576987729fd55d7d037943ee92112e2d9bcd
netra212 <netra200021kcbdr@gmail.com>
2026-01-16 15:17:19 +0000

    Double Pointers Multiple Pointers

---
+++ 04_Pointers/04_Double_Pointers_Multiple_Pointers.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+/**
+ *
+ * Double Pointers Multiple Pointers.
+    int n = 100;
+    int *p = &n;
+    If we want to store the address of the pointer in this scenarios, we used the double pointer.
+
+    int **ptr = &p;
+    int ***p1 = &ptr;
+
+    Use case of
+
+ */
+
+int main()
+{
+    //
+}
