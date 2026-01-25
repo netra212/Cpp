3378f030e81c9b1a6d519c7c2d5fa86ce44d191b
netra212 <netra200021kcbdr@gmail.com>
2026-01-25 16:55:59 +0000

    LinkedList introduction

--- 07_LinkedList/01_LinkedList_Intro_Traversal_and_Insertion.cpp
+++ 07_LinkedList/01_LinkedList_Intro_Traversal_and_Insertion.cpp
@@ -14,10 +14,43 @@ public:
     }
 };
 
+// Add the Node at the End using Recursion.
+// Creating a LinkedList using recursion by adding node at the end.
+Node *addNodeAtEnd(int arr[], int index, int size)
+{
+    // return type of the function is Node or address type.
+    if (index == size)
+    {
+        return NULL;
+    }
+
+    Node *temp;
+    temp = new Node(arr[index]);
+    temp->next = addNodeAtEnd(arr, index + 1, size);
+
+    return temp;
+}
+
+// Adding the Node at the begining of the LinkedList.
+Node *addNodeAtBegining(int arr[], int index, int size, Node *previousAddress)
+{
+    if (index == size)
+    {
+        return previousAddress;
+    }
+
+    Node *temp;
+    temp = new Node(arr[index]);
+    temp->next = previousAddress;
+    return addNodeAtBegining(arr, index + 1, size, temp);
+}
+
+//
 int main()
 {
-    Node *head;
-    head = new Node(4);
+    Node *head, *tail;
+    // head = new Node(4);
+    tail = head = NULL;
 
     // cout << head->data << endl;
     // cout << head->next << endl;
@@ -45,11 +78,67 @@ int main()
     Node *temp = head;
     while (temp != NULL)
     {
+        cout << " Printing values: " << endl;
         cout << temp->data << endl;
         temp = temp->next;
     };
 
     // Inserting at the last of the LinkedList.
+
+    // LinkedList is empty.
+    for (int i = 0; i < 5; i++)
+    {
+        if (head == NULL)
+        {
+            head = new Node(arr[i]);
+            tail = head;
+        }
+        else
+        {
+            tail->next = new Node(arr[i]);
+            tail = tail->next;
+        }
+    }
+
+    // Printing the values of the LinkedList.
+    Node *temp1;
+    temp1 = head;
+
+    while (temp1)
+    {
+        cout << temp1->data << " " << endl;
+        temp1 = temp1->next;
+    }
+
+    // Calling the function.
+    head = addNodeAtEnd(arr, 0, 5);
+    Node *temp2;
+    temp2 = head;
+
+    while (temp2)
+    {
+        cout << temp2->data << " " << endl;
+        temp2 = temp2->next;
+    }
+
+    //
+    head = addNodeAtBegining(arr, 0, 5, head);
+
+    // Inserting a Node at the Middle or Between the LinkedList or particularPosition.
+    int x = 3;
+    int value = 30;
+
+    Node *temp = head;
+    x--;
+
+    while (x--)
+    {
+        temp = temp->next;
+    }
+    Node *temp2;
+    temp2 = new Node(value);
+    temp2->next = temp->next;
+    temp->next = temp;
 }
 
 //
