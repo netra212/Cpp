f417737e38c84deccb5cfd7ff9d98757ad47d126
netra212 <netra200021kcbdr@gmail.com>
2026-01-30 18:40:28 +0000

    Doubly LL

--- 07_LinkedList/05_Doubly_LL.cpp
+++ 07_LinkedList/05_Doubly_LL.cpp
@@ -33,6 +33,11 @@ DoubleLinkedListNode *createDLLUsingRecursion(int arr[], int index, int size, Do
     return temp;
 }
 
+// Insert at any points.
+void InsertNodeAtAnyGivenPosition(int pos)
+{
+}
+
 //
 int main()
 {
@@ -82,6 +87,135 @@ int main()
             tail->next = temp;
             temp->previous = tail;
             tail = temp;
+        }
+    }
+
+    // Create Doubly LinkedList using Recursion.
+
+    // Insert at any given position.
+    int pos = 2;
+    // insert at start.
+    if (pos == 0)
+    {
+        // case1: LinkedList does not exit.
+        if (head == NULL)
+        {
+            head = new DoubleLinkedListNode(5);
+        }
+        // case2: LinkedList exist.
+        else
+        {
+            DoubleLinkedListNode *temp = new DoubleLinkedListNode(5);
+            temp->next = head;
+            head->previous = temp;
+            head = temp;
+        }
+    }
+    else
+    {
+        DoubleLinkedListNode *curr = head;
+        // Go to the node, after which i have to insert.
+        while (--pos)
+        {
+            curr = curr->next;
+        };
+        // insert at end.
+        if (curr->next == NULL) // Last node
+        {
+            DoubleLinkedListNode *temp = new DoubleLinkedListNode(5);
+            temp->previous = curr;
+            curr->next = temp;
+        }
+        else // Insert at Middle.
+        {
+            DoubleLinkedListNode *temp = new DoubleLinkedListNode(5);
+            temp->next = curr->next;
+            temp->previous = curr;
+            curr->next = temp;
+            temp->next->previous = temp;
+        }
+    }
+    // Delete at start.
+    if (head != NULL)
+    {
+        // If only one Node exist.
+        if (head->next == NULL)
+        {
+            delete head;
+            head = NULL;
+        }
+        // If more than 1 Node exist.
+        else
+        {
+            DoubleLinkedListNode *temp = head;
+            head = head->next;
+            delete temp;
+            head->previous = NULL;
+        }
+    }
+    // Delete at end.
+    if (head != NULL)
+    {
+        // If only 1 Node exist.
+        if (head->next == NULL)
+        {
+            delete head;
+            head = NULL;
+        }
+        // If more than 1 Node exist.
+        else
+        {
+            DoubleLinkedListNode *curr = head;
+            // current node laii last samma line.
+            while (curr->next)
+            {
+                curr = curr->next;
+            }
+
+            curr->previous->next = NULL;
+            delete curr;
+        }
+    }
+    // Delete at any given position.
+    int pos = 3;
+    // delete at start;
+    if (pos == 1)
+    {
+        // If only one Node exist.
+        if (head->next == NULL)
+        {
+            delete head;
+            head = NULL;
+        }
+        // If more than 1 Node exist.
+        else
+        {
+            DoubleLinkedListNode *temp = head;
+            head = head->next;
+            delete temp;
+            head->previous = NULL;
+        }
+    }
+    else
+    {
+        DoubleLinkedListNode *curr = head;
+        while (--pos)
+        {
+            curr = curr->next;
+        }
+        // delete at end;
+        if (curr->next == NULL)
+        {
+            //
+            curr->previous->next = NULL;
+            delete curr;
+        }
+        else
+        {
+            // delete at middle;
+            curr->previous->next = curr->next;
+            curr->next->previous = curr->previous;
+            delete curr;
         }
     }
 }
