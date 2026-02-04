75f8846f51e6547b5663e96a3e91732ca718492d
netra212 <netra200021kcbdr@gmail.com>
2026-02-04 15:12:43 +0000

    next smaller element

--- 08_Stack/03_Next_Greater_Problem.cpp
+++ 08_Stack/03_Next_Greater_Problem.cpp
@@ -25,6 +25,46 @@ vector<long long> nextGreaterElement(vector<long long> v1, int n)
     return ans;
 }
 
+// More optimized way of solving above question.
+vector<long long> nextGreaterElement2(vector<long long> &arr, int n)
+{
+    vector<long long> ans(n, -1);
+    stack<int> st;
+    for (int i = 0; i < n; i++)
+    {
+        while (!st.empty() && arr[st.top()] < arr[i])
+        {
+            ans[st.top()] = arr[i];
+            st.pop();
+        }
+        st.push(i);
+    }
+    return ans;
+
+    // Another methods.
+    // stack<int> st1;
+    // vector<long long> ans1(n, -1);
+    // for (int i = n - 1; i >= 0; i--)
+    // {
+    //     while (!st.empty() && arr[st.top()] < arr[i])
+    //     {
+    //         st.pop();
+    //     }
+    //     if (!st.empty())
+    //     {
+    //         ans1[i] = arr[st1.top()];
+    //     }
+    //     st.push(i);
+    // }
+    // return ans1;
+}
+
+// Q. Next smaller Elements on right side.
+vector<long long> *nextSmallerElementOnRightSide(vector<long long> arr1, int n)
+{
+    //
+}
+
 //
 int main()
 {
