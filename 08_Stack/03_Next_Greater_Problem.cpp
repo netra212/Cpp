389008e81098e067c36beba93a848aef3e12c0a7
netra212 <netra200021kcbdr@gmail.com>
2026-02-04 16:06:00 +0000

    stack span problem

--- 08_Stack/03_Next_Greater_Problem.cpp
+++ 08_Stack/03_Next_Greater_Problem.cpp
@@ -26,7 +26,7 @@ vector<long long> nextGreaterElement(vector<long long> v1, int n)
 }
 
 // More optimized way of solving above question.
-vector<long long> nextGreaterElement2(vector<long long> &arr, int n)
+vector<long long> nextGreaterElement2(vector<int> &arr, int n)
 {
     vector<long long> ans(n, -1);
     stack<int> st;
@@ -60,9 +60,72 @@ vector<long long> nextGreaterElement2(vector<long long> &arr, int n)
 }
 
 // Q. Next smaller Elements on right side.
-vector<long long> *nextSmallerElementOnRightSide(vector<long long> arr1, int n)
+vector<int> nextSmallerElementOnRightSide(vector<int> arr1, int n)
+{
+    stack<int> st;
+    vector<int> ans1(n, -1);
+    for (int i = 0; i < n; i++)
+    {
+        while (!st.empty() && arr1[st.top()] > arr1[i])
+        {
+            ans1[st.top()] = arr1[i];
+            st.pop();
+        }
+        st.push(i);
+    }
+    return ans1;
+}
+
+// Q. Smallest Number on Left.
+vector<int> smallestNumberLeft(vector<int> arr2, int n)
+{
+    stack<int> st2;
+    vector<int> ans2(n, -1);
+    for (int i = n - 1; i >= 0; i--)
+    {
+        while (!st2.empty() && arr2[st2.top()] > arr2[i])
+        {
+            ans2[st2.top()] = arr2[i];
+            st2.pop();
+        }
+        st2.push(i);
+    }
+    return ans2;
+}
+
+// Q. Nearest Greatest Number in Left.
+void findNearestGreatestNumberInLeft()
 {
     //
+}
+
+// Q. Stock Span Problem.
+vector<int> stockSpanProblem(vector<int> stockPrice, int n)
+{
+    /**
+     *
+     * stockPrice = [100, 80, 55, 70, 60, 75, 85]
+     * Find all the stock prices which is lesser than 75 stock price.
+     * Problem is similar to Next Greatest on Left side.
+     *
+     */
+    stack<int> st;
+    vector<int> ans(n, 1);
+    for (int i = n - 1; i >= 0; i--)
+    {
+        while (!st.empty() && stockPrice[i] > stockPrice[st.top()])
+        {
+            ans[st.top()] = st.top() - i;
+            st.pop();
+        }
+        st.push(i);
+    }
+    while (!st.empty())
+    {
+        ans[st.top()] = st.top() + 1;
+        st.pop();
+    }
+    return ans;
 }
 
 //
