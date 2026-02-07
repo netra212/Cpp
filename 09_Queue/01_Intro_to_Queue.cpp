41b24b232b31ce4567d9a21607b9f68820548f92
netra212 <netra200021kcbdr@gmail.com>
2026-02-07 23:03:03 +0000

    Introduction to Queue

---
+++ 09_Queue/01_Intro_to_Queue.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+void QueueIntroduction()
+{
+    /**
+     * Operation in Queue.
+     * a. Push
+     * b. pop
+     * c. front or start
+     * d. isFull
+     * e. isEmpty
+     *
+     *  front -> point at first person.
+     *  rear -> point the location of the last person or last ma kun manche ako cha teslaii point garcha.
+     *
+     */
+}
+
+int main()
+{
+    //
+}
