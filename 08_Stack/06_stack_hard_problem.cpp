894ee2c73f05b60756d042249a015688c83f06ab
netra212 <netra200021kcbdr@gmail.com>
2026-02-10 17:29:08 +0000

    Stack hard Problems

--- 08_Stack/06_stack_hard_problem.cpp
+++ 08_Stack/06_stack_hard_problem.cpp
@@ -7,7 +7,7 @@ class Solution
     /** store original value */
     stack<int> st1;
     /** store minimum value */
-    stack<int> st2;
+    // stack<int> st2;
     // % -> gives original element.
     // /(divide) -> gives minimum element.
 public:
@@ -57,6 +57,49 @@ public:
     }
     /** NOTE: In order to store two number at same place then we have to use the % (modulus operator).*/
 };
+
+// NEED TO RE-WATCHED THIS LECTURE.
+// Maximum of minimum for every window size.
+vector<int> MaximumOfMinimumForEveryWindowsize(vector<int> &arr, int n)
+{
+    /**
+     * [10, 20, 15, 50, 10, 70, 30]
+     * k = 3,
+     * {10, 20, 15} -> 10 (minimum for every 3 window size)
+     * {20, 15, 50} -> 15
+     * {15, 50, 10} -> 10
+     * {50, 10, 70} -> 10
+     * {10, 70, 30} -> 10
+     * so, we required vector to store answer.
+     * answer = []
+     * arr =  [10, 20, 15, 50, 10, 70, 30]
+     * for k = 1,
+     *  {10}, {20}, {15}, {50}, {10}, {70}, {30}
+     * for k = 2,
+     * {10, 20} -> 10 (min), {20, 15} -> 20 (min), {15, 50} -> 15 (min), {50, 10} -> 10 (min), {10, 70} -> 10 (min), {70, 30} -> 30 (Min)
+     * Minimum of 2 windows: 10, 20, 15, 10, 10, 30
+     * Now, the maximum of above minimum of window size is:  30
+     * Final Answer is: 30
+     */
+    // FIRST APPROACH N^3.
+    // vector<int> ans(n, 0);
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < n - 1; j++)
+    //     {
+    //         int num = INT_MAX;
+    //         for (int k = j; k < i + 1; k++)
+    //         {
+    //             num = min(num, arr[k]);
+    //         }
+
+    //         ans[i] = max(ans[i], num);
+    //     }
+    // }
+    // return ans;
+
+    // OPTIMIZED WAY.
+}
 
 //
 int main()
