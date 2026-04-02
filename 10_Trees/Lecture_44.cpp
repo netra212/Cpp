29aac90f65774c757d88b88dfee4450e65f5a050
netra212 <netra200021kcbdr@gmail.com>
2026-04-02 19:14:32 +0100

    Burning tree questions

--- 10_Trees/Lecture_44.cpp
+++ 10_Trees/Lecture_44.cpp
@@ -52,8 +52,63 @@ int maxPathSum(Node *root, int sum){
 Burning Tree
 Given the root of a binary tree and a target node, determine the minimum time required to burn the entire tree if the target node is set on fire. In one second, the fire spreads from a node to its left child, right child, and parent.
 */
+
+int Burn(Node *root, int target, int &timer){
+    if(!root){
+        return 0;
+    }
+
+    // Since this will be burning nodes. 
+    if(root->data == target){
+        return -1;
+    }
+    int left = Burn(root->left, target, timer);
+    int right = Burn(root->right, target, timer);
+
+    // If value of left is negative then left is burning. 
+    if(left<0){
+        timer = max(timer, abs(left) + right);
+        return left - 1;
+    }
+    // If value of right is negative then right is burning. 
+    else if(right <0){
+        timer = max(timer, left + abs(right));
+        return right - 1;
+    }
+    // If left and right both comes in positives then left and right are not burning yet. 
+    return max(left, right)+1;
+}
+void Find(Node *root, int target, Node * &temp){
+    if(!root){
+        return;
+    }
+
+    if(root->data == target){
+        temp = root;
+    }
+
+    Find(root->left, target, temp);
+    Find(root->right, target, temp);
+}
+
+int Height(Node * &temp){
+    if(!root){
+        return 0;
+    }
+    return 1 + max(Height(root->left), Height(root->right));
+}
+
+int minTime(Node *root, int target){
+    int timer = 0;
+    Burn(root, target, timer);
+    Node *temp;
+    Find(root, target, temp);
+    int num = Height(temp) - 1;
+    return max(timer, num);
+}
+
 int main(){
-    // 
+    //
 }
 
 
