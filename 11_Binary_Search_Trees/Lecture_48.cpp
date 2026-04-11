ccaf675f900d31fe94a00641af33195180f71ccd
netra212 <netra200021kcbdr@gmail.com>
2026-04-11 23:38:26 +0100

    deleting a node from a BST

---
+++ 11_Binary_Search_Trees/Lecture_48.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+/**
+// Question: 1: Delete a Node from a BST.
+
+Given a Binary Search Tree and a Node value X. Delete the Node with the given value X from the BST. If no Node with value x exist, then do not make any changes. 
+*/
+
+Node *deleteNode(Node *root, int x){
+    
+}
+
+int main(){
+
+}
