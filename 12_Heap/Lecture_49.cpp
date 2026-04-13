855f1f9d7f873d147bafff5638152494e1319d12
netra212 <netra200021kcbdr@gmail.com>
2026-04-13 06:29:23 +0100

    Heap Introductions

---
+++ 12_Heap/Lecture_49.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+/**
+# 
+Heap: is also a Priority Queue.
+- maxHeap: ParentNode > ChildNode. 
+- minHeap: ParentNode < ChildNode. 
+*/
+
+int main(){
+
+}
+
+
