02af30649bf476b26420df6f39a5416697353b73
netra212 <netra200021kcbdr@gmail.com>
2026-04-13 04:18:48 +0100

    AVL tree

--- 11_Binary_Search_Trees/NewAVL.cpp
+++ 11_Binary_Search_Trees/NewAVL.cpp
@@ -20,10 +20,18 @@ int getHeight(Node *root){
     return root->height;
 }
 
+void updateHeight(Node *root){
+    int leftHeight = getHeight(root->left);
+    int rightHeight = getHeight(root->right);
+    root->height = 1 + max(leftHeight, rightHeight);
+
+}
 Node *rotateRight(Node *root){
     Node *temp = root->left;
     root->left = temp->right;
     temp->right = root;
+    updateHeight(root);
+    updateHeight(temp);
     return temp;
 }
 
@@ -31,11 +39,47 @@ Node *rotateLeft(Node *root){
     Node *temp = root->right;
     root->right = temp->left;
     temp->left = root;
+    updateHeight(root);
+    updateHeight(temp);
     return temp;
 }
 
 Node *Balance(Node *root){
-    
+    if(!root){
+        return NULL;
+    }
+
+    // Update the height. 
+    updateHeight(root);
+
+    // Calculate the Balance factor.
+    int balance = getHeight(root->left) - getHeight(root->right);
+
+    // Balance > 1 means Left is Unbalance.
+    if(balance > 1){
+        // Left Left Unbalance. 
+        if(getHeight(root->left->left) >= getHeight(root->left->right)){
+            root = rotateRight(root);
+        }
+        // Left Right Unbalance. 
+        else{
+            root->left = rotateLeft(root->left);
+            root = rotateRight(root->right);
+        }
+    }
+    // Balanced < -1 means Right is Unbalance. 
+    else if(balance<-1){
+        // Right Right Unbalance. 
+        if(getHeight(root->right->right) >= getHeight(root->right->left)){
+            root = rotateLeft(root);
+        }
+        // Left Right Unbalance. 
+        else{
+            root->right = rotateRight(root->right);
+            root = rotateLeft(root);
+        }
+    }
+    return root;
 }
 
 Node *insertBST(Node *root, int value){
@@ -51,6 +95,24 @@ Node *insertBST(Node *root, int value){
     return Balance(root);
 }
 
+void inorder(Node *root){
+    if(!root){
+        return;
+    }
+    inorder(root->left);
+    count << root->data;
+    inorder(root->right);
+}
+
+void preOrder(Node *root){
+    if(!root){
+        return;
+    }
+    count << root->data;
+    inorder(root->left);
+    inorder(root->right);
+}
+
 int main(){
     Node *root = NULL;
     int value;
@@ -62,4 +124,9 @@ int main(){
             break;
         }
     };
+
+    inorder(root);
+    cout << endl;
+    preOrder(root);
+    return 0;
 }
