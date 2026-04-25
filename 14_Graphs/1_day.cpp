ac84ae7c0870a22fa835cd0352ea59f903a82053
netra212 <netra200021kcbdr@gmail.com>
2026-04-25 18:10:34 +0100

    graphs topics

---
+++ 14_Graphs/1_day.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+/**
+# Graph: Data Structure
+
+# Overview:
+1. What are Graph ?
+2. Types of Graph ?
+3. How to Implement ?
+4. Traversing a Graph. 
+    - BFS + Question. 
+    - DFS + Question. 
+5. Cycle Detection. 
+6. Topological sort. 
+    - khan's algorithm. 
+    - DFS - Based. 
+7. Flood Fill. 
+8. Connected Components. 
+9. Shortest Path Algorithm. 
+    - Single source (#3 algorithm).
+    - Multi - source
+10. DSU - Disjoint Union set. 
+11. MST - Minimum Spanning Tree.  
+    - Prim's. 
+    - kruskal. 
+12. Hamilton Path (TSP).
+13. Graph Colouring. 
+14. Strongly Connected Components. 
+15. Network flow. 
+    - Ford fulkerson. 
+    - Edmond's karp. 
+*/
+
+int main(){
+
+}
