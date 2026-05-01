3a0aca6f6a75106fa7606604dc1cb2ca4c6c5ee4
netra212 <netra200021kcbdr@gmail.com>
2026-05-01 22:49:51 +0100

    Find a center of a graph

--- 14_Graphs/Lecture_58.cpp
+++ 14_Graphs/Lecture_58.cpp
@@ -19,6 +19,27 @@ int minimumStep(int n){
     return count_edge;
 }
 
+
+/**
+Question:
+Find Center of star graph. 
+There is an undirected star graph consisting of n nodes labeled from 1 to n. A star graph is a graph where there is one center node and exactly n - 1 edges that connect the center node with every other node. 
+
+You are given 2D integer array edges where each edges[i] = [u_i, v_i] indicates that there is an edge between the nodes u_i and v_i. Return the center of the given star graph. 
+
+Input Edges = [[1, 2], [2, 3], [4, 2]]
+Output: 2
+
+Explanation: As shown in the figure above, node 2 is connected to every other node, so 2 is the center. 
+*/
+int findCenter(vector<vector<int>> & edges){
+    if(edges[0][0] == edges[1][0] || edge[0][0] == edges[1][1]){
+        return edge[0][0];
+    }
+    return edges[1][0];
+}
+
+
 int main(){
 
 }
