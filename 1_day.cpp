c93ecb70832a956df3e2697be6db0b70b1668733
netra212 <netra200021kcbdr@gmail.com>
2026-05-24 18:17:40 +0100

    graphs

--- 1_day.cpp
+++ 1_day.cpp
@@ -32,6 +32,29 @@ using namespace std;
     - Edmond's karp. 
 */
 
+vector<int> bfsOfGraph(int V, vector<int> adj[]){
+    vector<int> ans;
+    bool visited[V] = {0};
+    queue <int> q;
+    q.push(0);
+    visited[0] = 0;
+    int node;
+
+    while(!q.empty()){
+        node = q.front();
+        q.pop();
+        ans.push_back(node);
+        // Going through the BFS...
+        for(int i = 0; i < adj[node].size(); i++){
+            if(!visited[adj[node][i]]){
+                q.push(adj[node][i]);
+                visited[adj[node][i]] = 1;
+            }
+        }
+    }
+    return ans;
+}
+
 int main(){
 
 }
